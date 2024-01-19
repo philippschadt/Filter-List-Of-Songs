ccbbb9816ac51aeadd7c4ccfe33c5371ce281ed5
philippschadt <philippschadt@gmx.de>
2024-01-19 22:28:13 +0100

    Initial Commit

---
+++ Filter List Of Songs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Filter List Of Songs
+//
+//  Created by Philipp Schadt on 19.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Filter List Of Songs/Filter_List_Of_SongsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Filter_List_Of_SongsApp.swift
+//  Filter List Of Songs
+//
+//  Created by Philipp Schadt on 19.01.24.
+//
+
+import SwiftUI
+
+@main
+struct Filter_List_Of_SongsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
