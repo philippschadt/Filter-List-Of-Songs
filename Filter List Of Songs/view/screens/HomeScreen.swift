cc22aac1937ee8b55d8b55e01c3e28178ad2a64e
philippschadt <philippschadt@gmx.de>
2024-01-22 19:55:13 +0100

    Updated Files

--- Filter List Of Songs/view/screens/HomeScreen.swift
+++ Filter List Of Songs/view/screens/HomeScreen.swift
@@ -8,8 +8,65 @@
 import SwiftUI
 
 struct HomeScreen: View {
+    @StateObject private var mainViewModel = MainViewModel()
+    @AppStorage("filterCategory") var filterCategory = "artist"
+    
+    var filteredSongs: [Song] {
+        if mainViewModel.searchTerm.isEmpty {
+            return songs
+        } else {
+            return songs.filter{
+                $0.title.localizedCaseInsensitiveContains(mainViewModel.searchTerm) ||
+                $0.artist.localizedCaseInsensitiveContains(mainViewModel.searchTerm) ||
+                String($0.year).localizedCaseInsensitiveContains(mainViewModel.searchTerm)
+            }
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List(
+                mainViewModel.FilterList(
+                    filteredList: filteredSongs,
+                    filterCategory: filterCategory
+                )
+            ) { song in
+                VStack(alignment: .leading) {
+                    Text("Artist: \(song.artist)")
+                    Text("Title: \(song.title)")
+                    Text("Published: \(String(song.year))")
+                }.navigationTitle("Overview")
+                
+            } .searchable(
+                text: $mainViewModel.searchTerm,
+                prompt: "Search..."
+            )
+            
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Menu(content: {
+                        Button(
+                            "Artist(A-Z)",
+                            systemImage: filterCategory=="artist" ? "checkmark": "",
+                            action: {filterCategory = "artist"}
+                        )
+                        
+                        Button(
+                            "Title(A-Z)",
+                            systemImage: filterCategory=="title" ? "checkmark": "",
+                            action: {filterCategory = "title"}
+                        )
+                        
+                        Button(
+                            "Year(start with latest)",
+                            systemImage: filterCategory=="year" ? "checkmark": "",
+                            action: {filterCategory="year"}
+                        )
+                        
+                    }, label: {Label("",systemImage: "slider.vertical.3")})
+                }
+            }
+        }
     }
 }
 
--- Filter List Of Songs/viewModel/MainViewModel.swift
+++ Filter List Of Songs/viewModel/MainViewModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+final class MainViewModel: ObservableObject {
+    @Published var searchTerm = ""
+  
+    func FilterList(filteredList: [Song], filterCategory:String) -> [Song] {
+        switch filterCategory {
+        case  "artist":
+            filteredList.sorted{$0.artist < $1.artist}
+        case "title":
+            filteredList.sorted{$0.title < $1.title}
+        case "year":
+            filteredList.sorted{$0.year > $1.year}
+        default:
+            filteredList
+        }
+    }
+}
+
